e7d4c8fa1c498bd4606f17bcc157e4b211ad159d
Franz Darelle Macatangga <franzmacatangga2@gmail.com>
2020-02-23 10:52:26 +0800

    change title

--- resources/views/documents.blade.php
+++ resources/views/documents.blade.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Municipality Of Luisiana</title>
+<title>PCL - Laguna</title>
 <!-- CSS Files -->
 <link href="/css/custom.css" rel="stylesheet">
 <link href="/css/responsive.css" rel="stylesheet">
